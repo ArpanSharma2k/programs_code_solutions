7fe031e6a85564869dc0c9214dc1502480059747
Arpan Kumar Sharma <f20180580@pilani.bits-pilani.ac.in>
2019-10-08 08:38:57 +0530

    Add files via upload

--- max profit v imp.cpp
+++ max profit v imp.cpp
@@ -14,19 +14,19 @@ int main()
 		int a[k+1][n+1];
 		int maxdiff=-INT_MAX;
 		for(int i=0;i<=k;i++)
-		for(int j=0;j<=n;j++)
+		for(int j=0;j<n;j++)
 		{
 			if(i==0||j==0)
 			a[i][j]=0;
 			else
 			{
 				maxdiff=max(a[i-1][j-1]-ar[j-1],maxdiff);
-				a[i][j]=ar[j-1]+maxdiff
+				a[i][j]=max(a[i][j-1],ar[j-1]+maxdiff);
 			;}
 		}
 		/*for(int i=0;i<=k;i++,cout<<endl)
 		for(int j=0;j<=n;j++,cout<<" ")
 		cout<<a[i][j];*/
-		cout<<a[k][n]<<endl;
+		cout<<a[k][n-1]<<endl;
 	}
 }
