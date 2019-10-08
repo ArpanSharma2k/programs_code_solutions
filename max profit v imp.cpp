b70b1d21132e7a3703e90a3ca8b3ef85cbdae693
Arpan Kumar Sharma <f20180580@pilani.bits-pilani.ac.in>
2019-10-08 08:43:37 +0530

    Update max profit v imp.cpp

--- max profit v imp.cpp
+++ max profit v imp.cpp
@@ -13,16 +13,17 @@ int main()
 		cin>>ar[i];
 		int a[k+1][n+1];
 		int maxdiff=-INT_MAX;
-		for(int i=0;i<=k;i++)
-		for(int j=0;j<n;j++)
-		{
-			if(i==0||j==0)
-			a[i][j]=0;
-			else
-			{
+		for (int i = 0; i <= k; i++) 
+          a[i][0] = 0;
+    for (int j = 0; j <= n; j++) 
+        a[0][j] = 0; 
+		for(int i=1;i<=k;i++,maxdiff=-INT_MAX)
+		
+		for(int j=1;j<n;j++)
+	{
+	
 				maxdiff=max(a[i-1][j-1]-ar[j-1],maxdiff);
 				a[i][j]=max(a[i][j-1],ar[j-1]+maxdiff);
-			;}
 		}
 		/*for(int i=0;i<=k;i++,cout<<endl)
 		for(int j=0;j<=n;j++,cout<<" ")
